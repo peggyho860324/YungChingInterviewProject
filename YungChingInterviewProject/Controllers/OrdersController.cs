069992e832141b3d4fb2a0f09a7ef9e6f90242e2
Peggy Ho <peggyho860324@gmail.com>
2026-03-04 08:35:28 +0800

    feat: 加入客戶訂單管理endpoint

---
+++ YungChingInterviewProject/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YungChingInterviewProject.DTOs.Order;
+using YungChingInterviewProject.Services;
+
+namespace YungChingInterviewProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly OrderManagementService _service;
+
+        public OrdersController(OrderManagementService service) 
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery]QueryOrderRequest reuqest)
+        {
+            var result = await _service.QueryOrderAsync(reuqest);
+            return Ok(result);
+        }
+    }
+}
